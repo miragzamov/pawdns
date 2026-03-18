e31d68c11c695f368d8f8c28f54c19371e373da9
MIRAGZAMOV <miragzamov24@gmail.com>
2026-03-18 21:07:18 +0500

    Enhance branding by updating logo text styling and removing animation from sign-in button

--- src/app/_components/login-button.tsx
+++ src/app/_components/login-button.tsx
@@ -5,17 +5,11 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "~/components/ui/button";
 import { LogOut, Github } from "lucide-react";
 
-export function SignInButton({
-  children,
-  isAnimated,
-}: {
-  children: React.ReactNode;
-  isAnimated?: boolean;
-}) {
+export function SignInButton({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Button
-        className={`flex items-center gap-2 ${isAnimated && "animate-bounce"}`}
+        className="flex items-center gap-2"
         onClick={async () => {
           await signIn("github");
         }}
